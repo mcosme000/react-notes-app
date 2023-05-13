104d11ac5137d338fe008f18d3eb4e3b11b0c2a9
Maria Cosmen <mcosme00@gmail.com>
2023-05-13 20:07:48 +0900

    Setup react-router-dom and add NavLinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
-
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App m-auto rounded-lg bg-slate-300 w-4/5 h-4/5 shadow-md">
-      <Header />
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,40 @@
-import ListItem from "./ListItem"
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom';
+import classNames from "classnames";
+import ItemsContext from '../context/Items';
 
 const Header = () => {
+
+  const { setCurrentTab } = useContext(ItemsContext)
+
+  const navStyles = classNames('px-8 py-4', {
+    isActive: true,
+  });
+
+  const navigation = [
+    {id: 1, name: "All", href:"/"},
+    {id: 2, name: "Home", href: "/home"},
+    {id: 3, name: "Presents", href:"/presents"},
+    {id: 4, name: "Hobbies", href:"/hobbies"},
+    {id: 5, name: "Fashion", href:"/fashion"}
+  ]
+
+  const handleClick = (e) => {
+    setCurrentTab(e.target.innerHTML)
+  }
+
+  const renderNavigation = navigation.map((element) => {
+    return <NavLink
+      key={element.id}
+      to={element.href}
+      className={navStyles}
+      onClick={handleClick}>
+      {element.name}</NavLink>
+  })
   return (
     <header>
       <ul className="grid grid-cols-5 text-center">
-        <ListItem selected>All</ListItem>
-        <ListItem>Home</ListItem>
-        <ListItem>Presents</ListItem>
-        <ListItem>Hobbies</ListItem>
-        <ListItem>Add</ListItem>
+        {renderNavigation}
       </ul>
     </header>
   )
