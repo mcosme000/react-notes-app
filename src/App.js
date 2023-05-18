15f84c9d6b3b2db709156b2a10603eb3fa027ff2
Maria Cosmen <mcosme00@gmail.com>
2023-05-18 17:01:47 +0900

    Update favicon, title and description

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {Helmet} from "react-helmet";
 import Header from "./components/Header";
 import Overlay from "./components/Overlay";
 import ItemList from './components/ItemList';
@@ -18,6 +19,12 @@ function App() {
 
   return (
     <div className="App lg:w-4/5 md:h-4/5 shadow-md bg-white">
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>React Wishlist</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+          <meta name="description" content="A simple React Application that helps you to keep track of your wishlist" />
+      </Helmet>
       <BrowserRouter>
         <Header />
         <main className="h-full grow relative p-3 overflow-auto">
